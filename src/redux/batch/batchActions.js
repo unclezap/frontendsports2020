a64daca7d881f74cab8561455a35c80a177f34ef
unclezap <zeb.blackwell@gmail.com>
2020-04-29 05:03:50 -0700

    minor fixes

--- src/redux/batch/batchActions.js
+++ src/redux/batch/batchActions.js
@@ -28,6 +28,7 @@ export const fetchBatches = () => {
             if (data.error) {
                 dispatch(fetchBatchesFailure(data.error, data.exception))
             } else {
+                console.log(data)
                 dispatch(fetchBatchesSuccess(data))
             }
         })
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware} from 'redux'
 import rootUnion from './rootUnion';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootUnion, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootUnion, applyMiddleware(thunk))
+
+// , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export default store
