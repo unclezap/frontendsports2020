237fe532607b574c24903bd15f20e34c95b124bf
unclezap <zeb.blackwell@gmail.com>
2020-05-15 14:32:11 -0700

    removed /build from gitignore

---
+++ build/precache-manifest.ab0109b36d9c69580767dcce84aef41f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9c1f9c3636e44c9947ccda4d2dea20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "018ce3e131010aaa940f",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "42da52a6ca7e4e1c5073",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "018ce3e131010aaa940f",
+    "url": "/static/js/2.fd84445a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd84445a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42da52a6ca7e4e1c5073",
+    "url": "/static/js/main.6250d5af.chunk.js"
+  },
+  {
+    "revision": "b82115d53e1707ae9f1b",
+    "url": "/static/js/runtime-main.5ca33f48.js"
+  },
+  {
+    "revision": "cdc3f863a7d97ffc033ac5275b927f93",
+    "url": "/static/media/code.cdc3f863.gif"
+  },
+  {
+    "revision": "8c44dfda3b6ae07f22fb2329afef5640",
+    "url": "/static/media/helmetCatch.8c44dfda.gif"
+  },
+  {
+    "revision": "f8029882264044c4e5cb3ecca7ff9e3f",
+    "url": "/static/media/hourGlass.f8029882.gif"
+  },
+  {
+    "revision": "5bca6a229b8b88069b854f7b131ae019",
+    "url": "/static/media/stats.5bca6a22.gif"
+  },
+  {
+    "revision": "04db2f9aadb49a420530b1df2915ef68",
+    "url": "/static/media/twoHundredW.04db2f9a.gif"
+  }
+]);
